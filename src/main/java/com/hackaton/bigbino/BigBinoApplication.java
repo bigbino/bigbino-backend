dc09b22e8eb5e6a2d1b4439b2075806aae8d42d1
Pedro Cavalcante <pedroc@anchorloans.com>
2020-06-14 01:21:08 -0300

    inicializar projeto

---
+++ src/main/java/com/hackaton/bigbino/BigBinoApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.bigbino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigBinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/bigbino/BigBinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.bigbino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigBinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
