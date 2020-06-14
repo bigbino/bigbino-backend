96c9363ea47933513ae39436c576f6a13896d379
Pedro Cavalcante <pedroc@anchorloans.com>
2020-06-14 03:15:05 -0300

    adicionando camada de negocio

--- src/main/java/com/hackaton/bigbino/controller/CaminhoneiroController.java
+++ src/main/java/com/hackaton/bigbino/controller/CaminhoneiroController.java
@@ -2,9 +2,11 @@ package com.hackaton.bigbino.controller;
 
 import com.hackaton.bigbino.model.Caminhoneiro;
 import com.hackaton.bigbino.repository.CaminhoneiroRepository;
+import com.hackaton.bigbino.service.CaminhoneiroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,11 +15,17 @@ import java.util.List;
 public class CaminhoneiroController {
 
     @Autowired
-    private CaminhoneiroRepository caminhoneiroRepository;
+    private CaminhoneiroService caminhoneiroService;
 
     @GetMapping(value = "/caminhoneiros")
     public List<Caminhoneiro> getAll(){
-        return caminhoneiroRepository.findAll();
+        return caminhoneiroService.getAllCaminhoneiros();
     }
+
+    @GetMapping(value = "/caminhoneiro/{id}")
+    public Caminhoneiro getCaminhoneiroById(@PathVariable(value = "id") long id) throws Exception {
+        return caminhoneiroService.getCaminhoneiroID(id);
+    }
+
 
 }
